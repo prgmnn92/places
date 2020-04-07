ce97afccd47539efbe135f5cbeddbef09a100b87
Phillip Pargmann <pargmann92@gmail.com>
2020-04-07 08:05:25 +0200

    map functions

--- src/App.js
+++ src/App.js
@@ -1,73 +1,67 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import Grid from "@material-ui/core/Grid";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
 
-import EventPage from "./pages/event-page/event-page.component";
-import LoginSignUpPage from "./pages/login-signup-page/login-signup-page.component";
-import Navigation from "./components/navigation/navigation.component";
+import EventPage from './pages/event-page/event-page.component';
+import LoginSignUpPage from './pages/login-signup-page/login-signup-page.component';
+import Navigation from './components/navigation/navigation.component';
 
-import {
-  auth,
-  createUserProfileDocument,
-  getAllEvents,
-} from "./firebase/firebase";
-import { setCurrentUser, setAllEvents } from "./redux/actions";
+import { auth, createUserProfileDocument, getAllEvents } from './firebase/firebase';
+import { setCurrentUser, setAllEvents } from './redux/actions';
 
-import "./App.css";
+import './App.css';
 
 class App extends React.Component {
-  unsubscribeFromAuth = null;
+	unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    const { setCurrentUser, setAllEvents } = this.props;
+	componentDidMount() {
+		const { setCurrentUser, setAllEvents } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot((snapShot) => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data(),
-          });
-        });
-      }
+				userRef.onSnapshot((snapShot) => {
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data()
+					});
+				});
+			}
 
-      setCurrentUser(userAuth);
-    });
+			setCurrentUser(userAuth);
+		});
 
-    setAllEvents();
+		getAllEvents().then((events) => {
+			setAllEvents(events);
+		});
+	}
 
-    getAllEvents().then((events) => {
-      setAllEvents(events);
-    });
-  }
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <Grid container className="main">
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={EventPage} />
-          <Route exact path="/login" component={LoginSignUpPage} />
-        </Switch>
-      </Grid>
-    );
-  }
+	render() {
+		return (
+			<Grid container className="main">
+				<Navigation />
+				<Switch>
+					<Route exact path="/" component={EventPage} />
+					<Route exact path="/login" component={LoginSignUpPage} />
+				</Switch>
+			</Grid>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
-  user: state.user,
+	user: state.user
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-  setAllEvents: (events) => dispatch(setAllEvents(events)),
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+	setAllEvents: (events) => dispatch(setAllEvents(events))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/event-card/event-card.component.jsx
+++ src/components/event-card/event-card.component.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { Paper } from "@material-ui/core";
+import React from 'react';
+import { Paper, Grid } from '@material-ui/core';
 
-import "./event-card.styles.scss";
+import './event-card.styles.scss';
 
 const EventCard = ({ event }) => {
-  return (
-    <Paper className="event-card" elevation={3}>
-      <h3>{event.title}</h3>
-      <span className="small-text">{event.text}</span>
-      <span className="small-text">
-        {event.user.firstName + " " + event.user.lastName}
-      </span>
-    </Paper>
-  );
+	return (
+		<Paper className="event-card" elevation={3}>
+			<div className="image" />
+			<div className="content" />
+		</Paper>
+	);
 };
 
 export default EventCard;
